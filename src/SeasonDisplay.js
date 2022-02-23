6993239c34c089a4830fa25a8243a65e22b47eac
Jesse-Bett <jessekip21@gmail.com>
2022-02-23 14:08:35 +0300

    Added style sheet to SeasonDisplay

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,3 +1,4 @@
+import './SeasonDisplay.css';
 import React from "react";
 
 const seasonConfig = {
@@ -9,28 +10,30 @@ const seasonConfig = {
   winter: {
     seasonText: "Let's hit the beach",
     iconName: "sun",
-  },
+  }
 };
 
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
-    return lat > 0 ? "Summer" : "Winter"; //returning the result of the ternary expression.
+    return lat > 0 ? "summer" : "winter"; //returning the result of the ternary expression.
   } else {
-    return lat > 0 ? "Winter" : "Summer";
+    return lat > 0 ? "winter" : "summer";
   }
 };
+
 
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
   const {seasonText,iconName} = seasonConfig[season]
 
   return (
-    <div>
-      <i className={` massive ${iconName} icon`} />
+    <div className={`season-display ${season}`}>
+      <i className={` icon-left massive ${iconName} icon`} />
       <h1>{seasonText}</h1>
-      <i className={` massive ${iconName} icon`} />
+      <i className={`icon-right massive ${iconName} icon`} />
     </div>
   );
 };
 
 export default SeasonDisplay;
+
